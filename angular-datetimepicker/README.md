2bf48c1e36743e785d1eca0079c7dc580759faca
linjinying <junejinying@gmail.com>
2016-09-28 14:47:44 +0800

    add relative datetimepicker-group

--- angular-datetimepicker/README.md
+++ angular-datetimepicker/README.md
@@ -1,6 +1,6 @@
 ## angular-datetimepicker
-![angular-confirm](https://github.com/linjinying/angular-components/blob/master/angular-datetimepicker/screenshot.png)  
-
+![angular-datetimepicker](https://github.com/linjinying/angular-components/blob/master/angular-datetimepicker/screenshot.png)  
+![angular-datetimepicker](https://github.com/linjinying/angular-components/blob/master/angular-datetimepicker/screenshot2.png)  
 ### demo
 [click here](http://www.w3cin.com/demo/angular-components/angular-datetimepicker/)
 ### 说明
@@ -11,6 +11,7 @@
 - **limitEndTime** 限制允许选择的结束时间  
 - **clearBtn** 是否显示清除按钮  
 - **removeDatetimepicker** 移除该时间选择器的函数
+- **relativeGroup** 关联时间组合，见下面扩展
 
 ### 依赖
 
@@ -42,6 +43,60 @@ var app = angular.module("app", ["ui.datetimepicker"]);
 app.controller("ctrl", ['$scope', function($scope) {
     $scope.options = {
         format: "yyyy-mm-dd"
+    };
+}]);
+```
+
+### 扩展
+考虑到时间组合起来使用的频率是非常高，这里扩展了一个API`relativeGroup`函数，用于限制时间组合的时间可选范围，函数返回对象的例子如下:  
+```json
+{
+	groudId:"startTime",//或endTime,标识组件是开始时间还是结束时间
+	relativeDatetime:scope.endTime,//或scope.startTime,关联另外一个scope
+	relativeOptions:$scope.endTimeOptions//或scope.startTimeOptions,关联另外一个配置项目
+}
+```
+**html代码**  
+```html
+<body ng-app="app" ng-controller="ctrl">
+    <form class="form-inline" role="form">
+        <div class="form-group">
+            <label class="control-label">请选择：</label>
+            <input type="text" datetimepicker="startTimeOptions" class="form-control" name="startTime" ng-model="startTime" placeholder="请输入开始时间" required readonly="readonly" />
+            <label class="control-label">-</label>
+            <input type="text" datetimepicker="endTimeOptions" class="form-control" name="endTime" ng-model="endTime" placeholder="请输入结束时间" required readonly="readonly" />
+        </div>
+    </form>
+</body>
+```
+
+**angular代码**
+```javascript 
+var app = angular.module("app", ["ui.datetimepicker"]);
+app.controller("ctrl", ['$scope', function($scope) {
+    $scope.startTimeOptions = {
+        format: "yyyy-mm-dd",
+        limitStartTime:"2016-09-02",
+        limitEndTime:"2016-09-22",
+        relativeGroup: function() {
+            return {
+                groupId:"startTime",
+                relativeDatetime: $scope.endTime,
+                relativeOptions: $scope.endTimeOptions
+            }
+        }
+    };
+    $scope.endTimeOptions = {
+        format: "yyyy-mm-dd",
+        limitStartTime:"2016-09-01",
+        limitEndTime:"2016-09-30",
+        relativeGroup: function() {
+            return {
+                groupId:"endTime",
+                relativeDatetime: $scope.startTime,
+                relativeOptions: $scope.startTimeOptions
+            }
+        }
     };
 }]);
 ```
--- angular-datetimepicker/dist/js/angular-datetimepicker.js
+++ angular-datetimepicker/dist/js/angular-datetimepicker.js
@@ -9,8 +9,9 @@ angular.module('ui.datetimepicker', [])
             link: function(scope, element, attrs, ngModelCtrl) {
                 var options = scope.options || {};
                 var format = options.format || "yyyy-mm-dd";
-
                 var dateOptions = {};
+                var LIMIT_START_TIME = options.LIMIT_START_TIME = angular.copy(options.limitStartTime);
+                var LIMIT_END_TIME = options.LIMIT_END_TIME = angular.copy(options.limitEndTime);
                 var isExist = {};
                 isExist.year = format.indexOf("yyyy") >= 0;
                 isExist.month = format.indexOf("mm") >= 0;
@@ -19,9 +20,9 @@ angular.module('ui.datetimepicker', [])
                 isExist.mintue = format.indexOf("ii") >= 0;
                 isExist.second = format.indexOf("ss") >= 0;
                 dateOptions = angular.extend({
-                    weekStart:1,
-                    todayBtn:true,
-                    todayHighlight:true,
+                    weekStart: 1,
+                    todayBtn: true,
+                    todayHighlight: true,
                     customIcon: true,
                     format: format,
                     autoclose: 1,
@@ -31,30 +32,30 @@ angular.module('ui.datetimepicker', [])
                     clearBtn: options.clearBtn !== false
                 }, dateOptions);
                 var viewMap = {
-                    yearView:4,
-                    monthView:3,
-                    dayView:2,
-                    hourView:1,
-                    mintueView:0
+                    yearView: 4,
+                    monthView: 3,
+                    dayView: 2,
+                    hourView: 1,
+                    mintueView: 0
                 };
-                if(isExist.year){
-                    dateOptions.startView = 4; 
+                if (isExist.year) {
+                    dateOptions.startView = 4;
                     dateOptions.minView = 4;
                     dateOptions.maxView = 4;
-                    if(isExist.month){
-                        dateOptions.startView = 3; 
+                    if (isExist.month) {
+                        dateOptions.startView = 3;
                         dateOptions.minView = 3;
                         dateOptions.maxView = 4;
-                        if(isExist.day){
-                            dateOptions.startView = 2; 
+                        if (isExist.day) {
+                            dateOptions.startView = 2;
                             dateOptions.minView = 2;
                             dateOptions.maxView = 4;
-                            if(isExist.hour){
-                                dateOptions.startView = 2; 
+                            if (isExist.hour) {
+                                dateOptions.startView = 2;
                                 dateOptions.minView = 1;
                                 dateOptions.maxView = 4;
-                                if(isExist.mintue){
-                                    dateOptions.startView = 2; 
+                                if (isExist.mintue) {
+                                    dateOptions.startView = 2;
                                     dateOptions.minView = 0;
                                     dateOptions.maxView = 4;
                                 }
@@ -63,11 +64,9 @@ angular.module('ui.datetimepicker', [])
                     }
                 }
                 /*
-                *解决日期选择器双击不能打开的bug
-                */
-                element.datetimepicker(dateOptions).on("hide", function() {
-                    $(element).blur();
-                });
+                 *初始化组件
+                 */
+                var datetimepicker = element.datetimepicker(dateOptions);
                 var limitStartTimeWatcher = scope.$watch('options.limitStartTime', function(currentValue, lastValue) {
                     if (currentValue !== lastValue || typeof lastValue !== "undefined") {
                         element.datetimepicker("setStartDate", currentValue);
@@ -79,13 +78,84 @@ angular.module('ui.datetimepicker', [])
                     }
                 });
                 /*
-                *移除日期选择器
-                */
+                 *移除日期选择器
+                 */
                 options.removeDatetimepicker = function() {
-                    element.datetimepicker("remove");
-                    limitStartTimeWatcher();
-                    limitEndTimeWatcher();
-                }
+                        element.datetimepicker("remove");
+                        limitStartTimeWatcher();
+                        limitEndTimeWatcher();
+                    }
+                    /*
+                     *事件注册
+                     */
+                var totalEvents = ["show", "hide", "changeDate", "changeYear", "changeMonth", "outOfRange", "next"];
+                angular.forEach(totalEvents, function(eventItem) {
+                    var onEventName = "on" + eventItem[0].toUpperCase() + eventItem.slice(1);
+                    if (angular.isFunction(options[onEventName])) {
+                        datetimepicker.on(eventItem, options[onEventName]);
+                    }
+                });
+                /*
+                 *利用hide event做一些hooks:
+                 */
+                var resetDatetimeGroupRange = function() {
+                    var datetimeGroup = options.relativeGroup();
+                    var groupId = datetimeGroup.groupId;
+                    var relativeDatetime = datetimeGroup.relativeDatetime;
+                    var relativeOptions = datetimeGroup.relativeOptions;
+                    var currentScopeValue = ngModelCtrl.$modelValue;
+                    switch (groupId) {
+                        case "startTime":
+                            if (currentScopeValue && relativeOptions.LIMIT_START_TIME) {
+                                var currentScopeValueOf = new Date(currentScopeValue.replace(' ', 'T')).valueOf();
+                                var limitStartTimeValueOf = new Date(relativeOptions.LIMIT_START_TIME.replace(' ', 'T')).valueOf();
+                                relativeOptions.limitStartTime = currentScopeValueOf > limitStartTimeValueOf ? currentScopeValue : relativeOptions.LIMIT_START_TIME;
+                            } else {
+                                relativeOptions.limitStartTime = relativeOptions.LIMIT_START_TIME || currentScopeValue;
+                            }
+                            if (relativeDatetime && LIMIT_END_TIME) {
+                                var relativeDatetimeValueOf = new Date(relativeDatetime.replace(' ', 'T')).valueOf();
+                                var limitEndTimeValueOf = new Date(LIMIT_END_TIME.replace(' ', 'T')).valueOf();
+                                options.limitEndTime = limitEndTimeValueOf < relativeDatetimeValueOf ? LIMIT_END_TIME : relativeDatetime;
+                            } else {
+                                options.limitEndTime = LIMIT_END_TIME || relativeDatetime;
+
+                            }
+                            break;
+                        case "endTime":
+                            if (currentScopeValue && relativeOptions.LIMIT_END_TIME) {
+                                var currentScopeValueOf = new Date(currentScopeValue.replace(' ', 'T')).valueOf();
+                                var limitEndTimeValueOf = new Date(relativeOptions.LIMIT_END_TIME.replace(' ', 'T')).valueOf();
+                                relativeOptions.limitEndTime = limitEndTimeValueOf < currentScopeValueOf ? relativeOptions.LIMIT_END_TIME : currentScopeValue;
+                            } else {
+                                relativeOptions.limitEndTime = relativeOptions.LIMIT_END_TIME || currentScopeValue;
+                            }
+                            if (relativeDatetime && LIMIT_START_TIME) {
+                                var relativeDatetimeValueOf = new Date(relativeDatetime.replace(' ', 'T')).valueOf();
+                                var limitStartTimeValueOf = new Date(LIMIT_START_TIME.replace(' ', 'T')).valueOf();
+                                options.limitStartTime = relativeDatetimeValueOf > limitStartTimeValueOf ? relativeDatetime : LIMIT_START_TIME;
+                            } else {
+                                options.limitStartTime = LIMIT_START_TIME || relativeDatetime;
+                            }
+                            break;
+                        default:
+                            ;
+                    }
+                    scope.$apply();
+                };
+                var fixDblick = function() {
+                    $(element).blur();
+                };
+                datetimepicker.on("hide", function() {
+                    /*
+                     *解决日期选择器双击不能打开的bug
+                     */
+                    fixDblick();
+                    /*
+                     *时间日期组合可选的边界
+                     */
+                    resetDatetimeGroupRange();
+                });
             }
         }
     }]);
