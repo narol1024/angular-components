7deea271f970b3bf1eac9411ad54a51f47d667b5
linjinying <junejinying@gmail.com>
2016-10-07 23:03:22 +0800

    +angular-circle-slider

--- angular-checkbox/src/js/angular-checkbox.js
+++ angular-checkbox/src/js/angular-checkbox.js
@@ -31,7 +31,7 @@ angular.module('ui.checkbox', [])
                 });
                 scope.$on('$destroy', function() {
                     valueWatcher();
-                })
+                });
             }
         };
     }]);
